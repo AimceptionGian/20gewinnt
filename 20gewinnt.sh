8c59eb421406e7b9f93e2eb5347fc7e7eaf34b01
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-04-03 16:08:45 +0200

    Spieler-Eingabe prüfen

--- 20gewinnt.sh
+++ 20gewinnt.sh
@@ -4,3 +4,20 @@
 #date: 03.04.24
 #author: Gian Oechslin
 #usage: -
+
+# Spiel beginnt mit 1
+spielstand=1
+echo "Der Computer beginnt das Spiel mit $spielstand."
+
+# Eingabe prüfen
+while true; do
+    read -p "Geben Sie eine gültige Zahl ein: " eingabe
+    if [[ $eingabe -gt 20 ]]; then
+        echo "Ihre Eingabe ist zu gross."
+    elif [[ $eingabe -eq $(($spielstand + 1))]] \
+      || [[ $eingabe -eq $(($spielstand + 2))]]; then
+        break
+    else
+        echo "Geben Sie eine gültige Zahl ein. Entweder meine Zahl + 1 oder + 2."
+    fi
+done

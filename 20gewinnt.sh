577efae94d253aa867c17598e809164dc8f802b7
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-04-07 11:54:18 +0200

    Spielbar gemacht

--- 20gewinnt.sh
+++ 20gewinnt.sh
@@ -9,39 +9,45 @@
 spielstand=1
 echo "Der Computer beginnt das Spiel mit $spielstand."
 
-# Eingabe prüfen
+# Endlosschleife
 while true; do
-    read -p "Geben Sie eine gültige Zahl ein: " eingabe
-    if [[ $eingabe -gt 20 ]]; then
-        echo "Ihre Eingabe ist zu gross."
-    elif [[ $eingabe -eq $(($spielstand + 1)) ]] \
-      || [[ $eingabe -eq $(($spielstand + 2)) ]]; then
-        break
-    else
-        echo "Geben Sie eine gültige Zahl ein. Entweder meine Zahl + 1 oder + 2."
+
+    # Eingabe prüfen
+    while true; do
+        read -p "Geben Sie eine gültige Zahl ein: " eingabe
+        if [[ $eingabe -gt 20 ]]; then
+            echo "Ihre Eingabe ist zu gross."
+        elif [[ $eingabe -eq $(($spielstand + 1)) ]] \
+        || [[ $eingabe -eq $(($spielstand + 2)) ]]; then
+            break
+        else
+            echo "Geben Sie eine gültige Zahl ein. Entweder meine Zahl + 1 oder + 2."
+        fi
+    done
+
+    # Eingabe ist gültig, also wird der Spielstand angepasst
+    spielstand=$eingabe
+
+    # Prüfen ob das Spiel gewonnen wurde
+    if [[ $spielstand -eq 20 ]]; then
+        echo "Sie haben das Spiel gewonnen."
+        exit 0
     fi
+
+    # Prüfen ob der Computer das Spiel gewinnen kann
+    if [[ $spielstand -gt 17 ]]; then
+        spielstand=20
+        echo "$spielstand" 
+        echo "Der Computer hat das Spiel gewonnen"
+        exit 1
+    # Computer wählt ideale Zahl
+    elif [[ $(((($spielstand + 1) % 3) - 1)) -eq 1 ]]; then
+        spielstand=$(($spielstand + 1))
+    elif [[ $(((($spielstand + 2) % 3) - 1)) -eq 1 ]]; then
+        spielstand=$(($spielstand + 2))
+    else
+        spielstand=$(($spielstand + $[RANDOM % 2] + 1))
+    fi
+    echo "$spielstand"
+
 done
-
-# Eingabe ist gültig, also wird der Spielstand angepasst
-spielstand=$eingabe
-
-# Prüfen ob das Spiel gewonnen wurde
-if [[ $spielstand -eq 20 ]]; then
-    echo "Sie haben das Spiel gewonnen."
-    exit 0
-fi
-
-# Prüfen ob der Computer das Spiel gewinnen kann
-if [[ $spielstand -gt 17 ]]; then
-    spielstand=20
-    echo "$spielstand \n Der Computer hat das Spiel gewonnen"
-    exit 1
-# Computer wählt ideale Zahl
-elif [[ $(((($spielstand + 1) % 3) - 1)) -eq 1 ]]; then
-    spielstand=$(($spielstand + 1))
-elif [[ $(((($spielstand + 2) % 3) - 1)) -eq 1 ]]; then
-    spielstand=$(($spielstand + 2))
-else
-    spielstand=$(($spielstand + $[RANDOM % 2] + 1))
-fi
-echo "$spielstand"
